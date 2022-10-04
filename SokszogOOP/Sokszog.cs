a95624bb7bc60c3f5532d3b89c19e9bae6d09e20
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-04 17:20:03 +0200

    Sokszög osztály alapállapot

---
+++ SokszogOOP/Sokszog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokszogOOP
+{
+    internal class Sokszog
+    {
+        private double a;
+
+        public Sokszog(double a)
+        {
+            this.a = a;
+        }
+
+        public double A { get => this.a; set => this.a = value; }
+
+
+    }
+}
